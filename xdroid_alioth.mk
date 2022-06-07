222bb5383335484ce25bd47fd53b3680929f352b
SawaMEN <az09209@yandex.ru>
2022-06-07 12:04:10 +0700

    Update blobs from MIUI V13.0.6.0.SKHEUXM

--- xdroid_alioth.mk
+++ xdroid_alioth.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The Potato Open Sauce Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,13 +8,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/xdroid/config/common.mk)
+
+XDROID_BOOT_DARK := true
+XDROID_BOOT := 1080
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+# Boot resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := xdroid_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -23,6 +29,6 @@ PRODUCT_MODEL := POCO F3
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V13.0.3.0.SKHMIXM release-keys"
+    PRIVATE_BUILD_DESC="alioth_global-user 12 RKQ1.200826.002 V13.0.6.0.SKHEUXM release-keys"
 
-BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/RKQ1.200826.002/V13.0.3.0.SKHMIXM:user/release-keys
+BUILD_FINGERPRINT := POCO/alioth_global/alioth:12/RKQ1.200826.002/V13.0.6.0.SKHEUXM:user/release-keys
